1548e3bbea291f9a208dbd3539e894d9b436ccd1
icyveins7 <icyveins@hotmail.com>
2020-04-15 11:22:24 +0800

    just finished indexed draws
    up to part 15.

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -12,7 +12,14 @@
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
+const float toRadians = 3.14159265f / 180.0f;
+float curAngle = 0.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -25,31 +32,48 @@ static const char* vShader = "             \n\
 											\n\
 layout (location = 0) in vec3 pos;         \n\
 											\n\
+out vec4 vCol;								\n\
+											\n\
 uniform mat4 model;						\n\
 											\n\
 void main(){								\n\
-	gl_Position = model * vec4(0.4*pos.x, 0.4*pos.y, pos.z, 1.0);     \n\
+	gl_Position = model * vec4(pos, 1.0);     \n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f); \n\
 }";
 
 // fragment shader self
 static const char* fShader = "             \n\
 #version 330                               \n\
 											\n\
+in vec4 vCol;								\n\
+											\n\
 out vec4 colour;					         \n\
 											\n\
 void main(){								\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);     \n\
+	colour = vCol;     \n\
 }";
 
 void CreateTriangle() {
+	unsigned int indices[] = { 
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	GLfloat vertices[] = {
 		-1.0f, -1.0f , 0.0f,
+		0.0f, -1.0f, 1.0f,
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -58,9 +82,10 @@ void CreateTriangle() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 	glBindVertexArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType) {
@@ -167,6 +192,8 @@ int main() {
 		return 3;
 	}
 
+	glEnable(GL_DEPTH_TEST); // for depth-testing
+
 	// Setup viewport size
 	glViewport(0, 0, bufferwidth, bufferheight);
 
@@ -190,21 +217,42 @@ int main() {
 			direction = !direction;
 		}
 
+		curAngle += 0.01f;
+		if (curAngle >= 360) {
+			curAngle -= 360;
+		}
+
+		if (sizeDirection) {
+			curSize += 0.0001f;
+		}
+		else {
+			curSize -= 0.0001f;
+		}
+
+		if (curSize >= maxSize || curSize <= minSize) {
+			sizeDirection = !sizeDirection;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
 		// unbind
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 		// unbind
 		glUseProgram(0);
